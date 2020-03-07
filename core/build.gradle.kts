91f09ec7301458cd8ffc598026192a256b1e6b06
Vadim Salavatov <xremmargorpx@gmail.com>
2020-03-07 15:02:32 +0300

    add automata library dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,9 +4,11 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     implementation("org.eclipse.rdf4j:rdf4j-client:$rdf4j")
+    implementation("dk.brics:automaton:1.12-1")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    testRuntimeOnly("org.slf4j:slf4j-simple:1.7.25")
 }
 
 tasks.test {
